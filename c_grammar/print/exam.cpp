53eff0303581d9cb91d2ec22777f0d34b56e14fb
YoungjunKim <color99b@naver.com>
2023-02-13 17:18:00 +0900

    code up 40번

--- c_grammar/print/exam.cpp
+++ c_grammar/print/exam.cpp
@@ -1,12 +1,58 @@
+// #include<stdio.h>
+
+// int main(){
+//     int a;
+//     scanf("%1d", &a);
+//         printf("[%d]\n", a*10000);
+//     for(int i=10; a != '\0';){
+//         a=0;
+//         scanf("%1d", &a);
+//         if(a*10000/i !=0){
+//         printf("[%d]\n", a*10000/i);
+//         }
+//         i = i * 10;
+//     }
+    
+
+//     return 0;
+
+// }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+//     int y; int m; int d;
+//     scanf("%d.%d.%d", &y, &m, &d);
+//     printf("%02d-%02d-%04d", d, m, y);
+
+
+//     return 0;
+
+// }
+
 #include<stdio.h>
 
 int main(){
-    char n[30];
-    scanf("%s", n);
-    for(int i=0; n[i] != '\0'; i++){
-        printf("\'%c\'\n", n[i]);
-    }
+   char a;
+    scanf("%c", &a);
+    printf("%c", a+1);
+
+
+    return 0;
 
 }
 
 
+// #include<stdio.h>
+
+// int main(){
+//    long long int a; long long int b;
+//     scanf("%lld %lld", &a,&b);
+//     printf("%lld", a+b);
+
+
+//     return 0;
+
+// }
